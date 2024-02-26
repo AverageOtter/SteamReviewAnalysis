9f69e3c8029e221f9686177c3f690485c79ac4c2
Tim Lyons <amazingasriel2001@gmail.com>
2024-02-25 21:58:38 -0800

    Improved Tokenizer 128->512, and some small analytics.

--- pythonTestingArea/sentimentAnalysis.py
+++ pythonTestingArea/sentimentAnalysis.py
@@ -1,22 +1,28 @@
-from transformers import pipeline
+from transformers import AutoModelForSequenceClassification
+from transformers import TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoConfig
+import numpy as np
+from scipy.special import softmax
+# Preprocess text (username and link placeholders)
+def preprocess(text):
+    return text
 
-sentiment_pipeline = pipeline("sentiment-analysis", model="finiteautomata/bertweet-base-sentiment-analysis")
-
-
-def SentimentAI(data):
-    return sentiment_pipeline(data)
-
-def main():
-    data = ["I Love you", "I hate you", "This is a house", "After 8 years playing it, I didn't improve my skills in-game.\n\nHowever, I learned new language skills: now I can curse in Russian and Brazilian Portuguese.\n\nAlso, my tolerance level to guttural screams and loud sounds was increased.\n\nNow I can bear the crying of my children for longer and I can make them feel better by singing sweet traditional songs from other cultures, such as \u041e\u0447\u0438 \u0447\u0451\u0440\u043d\u044b\u0435 (Dark Eyes) or Garota de Ipanema (The Girl from Ipanema).\n\nIn conclusion, Counter Strike transformed me into a better father and a better person, in addition to making me more patient, tolerant to hateful people and more trained in languages and cultures.\n\nBetter than any psychiatric therapy. 10/10"]
-    res= sentiment_pipeline(data)
-    dic = {}
-    for key in data:
-        for value in res:
-            dic[key] = value
-            res.remove(value)
-            break
-    print(dic)
-
-if __name__ == "__main__":
-    main()
+def sentimentAnal(data: list[str]) -> list:
+    MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    config = AutoConfig.from_pretrained(MODEL)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+    Sentiment = []
+    for rev in data:
+        text = preprocess(rev)
+        encoded_input = tokenizer(text, return_tensors='pt', truncation=True, max_length=512)
+        output = model(**encoded_input)
+        scores = output[0][0].detach().numpy()
+        scores = softmax(scores)
+        predicted_label_index = np.argmax(scores)  # Get the index of the label with highest probability
+        predicted_label = config.id2label[predicted_label_index]  # Get the label corresponding to the index
+        predicted_probability = scores[predicted_label_index]  # Get the probability of the predicted label
+        Sentiment.append((predicted_label, predicted_probability, rev))  # Append review, label, and probability as a tuple to Sentiment list
+    return Sentiment
     
+# print(sentimentAnal(["Hello this is a house", "I hate you", "I love you"]))
