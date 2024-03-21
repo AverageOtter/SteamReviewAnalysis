1f7dd0f9fc8fe28d3f73c38082990756478088f4
Tim Lyons <amazingasriel2001@gmail.com>
2024-03-20 22:35:16 -0700

    Cleaned up requirements, tested server with full process. neeed to display appropiately

--- core/views.py
+++ core/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages  # Import messages framework for displaying temporary messages
+# from django.contrib import messages  # Import messages framework for displaying temporary messages
 from .forms import InputForm
+import steam_ai_package
 
 def home(request):
     if request.method == 'POST':
         form = InputForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['gamename']
-            processed_data = name
+            # 
+            processed_data = steam_ai_package.get_sentiment_of_game(name)
             # Store the processed data in the session
             request.session['processed_data'] = processed_data
             # Redirect to the same page to avoid form resubmission
@@ -19,6 +21,6 @@ def home(request):
             # Delete the processed data from the session after displaying it
             del request.session['processed_data']
             # Add a success message to be displayed after the redirect
-            messages.success(request, 'Form submitted successfully!')
+            # messages.success(request, 'Form submitted successfully!')
 
     return render(request, 'index.html', {'form': form, 'processed_data': processed_data})
